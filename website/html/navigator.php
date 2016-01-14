be467526d879cf3a714b0f112e2dcfc01f266659
Jeremy Hicks <jerhicks@cisco.com>
2016-01-14 09:06:59 +0000

    Enable index.html

--- website/html/navigator.php
+++ website/html/navigator.php
@@ -1,12 +1,17 @@
 <?php 
 include 'navigatorItem.php';
-$items = [
-		new NavigatorItem("home"),
-		new NavigatorItem("contacts"),
-		new NavigatorItem("fixtures"),
-		new NavigatorItem("results"),
-		new NavigatorItem("tables"),
-		new NavigatorItem("averages"),
+$items = 
+[
+	new NavigatorItem("home"),
+	new NavigatorItem("contacts"),
+	new NavigatorItem("fixtures"),
+	new NavigatorItem("results"),
+	new NavigatorItem("tables"),
+	new NavigatorItem("averages"),
+	new NavigatorItem("resources"),
+	new NavigatorItem("rules"),
+	new NavigatorItem("records"),
+	new NavigatorItem("archive"),
 ]
 ?>
 <ul class="navigator">
